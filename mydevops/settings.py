3f025b2b2091dd37f10f4ebf0aac16f785a1767b
liangey <291406980@qq.com>
2018-12-24 17:46:47 +0800

    ley commit

--- mydevops/settings.py
+++ mydevops/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p1bq)@8*k#^62i7uxk4i%6apq-8rkb^cb!ftad44@no-q^mji8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_web',
 ]
 
 MIDDLEWARE = [
--- mydevops/urls.py
+++ mydevops/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import url
+from django.urls import path
+from django_web.views import index
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-
+    path(r'admin/', admin.site.urls),
+    path(r'^index/', index),
 ]
