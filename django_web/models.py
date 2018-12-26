2a5e65cb30056d267a2a7ef0785d88479230ada6
liangey <291406980@qq.com>
2018-12-26 09:18:46 +0800

    ley

--- django_web/models.py
+++ django_web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+'''
+models.CharField
+models.IntegerField
+models.FloatField
+models.AutoField
+models.TextField
+models.EmailField
+models.DateField
+models.DateTimeField
+models.ImageField
+models.URLField
+models.IPAddressField
+'''
